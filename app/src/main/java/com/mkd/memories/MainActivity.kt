523ba94e114f0c2c39723ab71b553a62ec3f3942
Dinesh Prasanth M K <mkdineshprasanth@gmail.com>
2024-03-10 18:19:26 -0700

    Setup hilt for development + upgrade gradle to 8.5
    Signed-off-by: Dinesh Prasanth M K <mkdineshprasanth@gmail.com>

--- app/src/main/java/com/mkd/memories/MainActivity.kt
+++ app/src/main/java/com/mkd/memories/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mkd.memories.ui.theme.Memories2Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/mkd/memories/MemoriesApplication.kt
@@ -0,0 +1,11 @@
+package com.mkd.memories
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * [Application] class for Memories
+ */
+@HiltAndroidApp
+class MemoriesApplication : Application() {
+}
