e18a6d1b1ebd2212e827e569460a6eb12f310b7d
Dinesh Prasanth M K <mkdineshprasanth@gmail.com>
2025-09-14 16:40:04 -0500

    Fix Hilt vm creation import warnings

--- feature/login/src/main/kotlin/com/mkd/memories/login/LoginScreen.kt
+++ feature/login/src/main/kotlin/com/mkd/memories/login/LoginScreen.kt
@@ -33,7 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mkd.memories.core.auth.util.RequestAuthTokenContract
 import com.mkd.memories.designsystem.theme.MemoriesTheme
